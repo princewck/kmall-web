b37fe8aec2b0c23e8bd03d4ad4f3f85c046bc97d
chengkai.wang <trickle.wang@petkit.com>
2017-01-13 01:19:43 +0800

    main.js http// -> //

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,7 +10,7 @@ require.config({
         'angular-require': BOWER_DIR + '/angular-require/angular-require.min',
         'angular-ui-router': BOWER_DIR + '/angular-ui-router/release/angular-ui-router.min',
         'angular-touch': BOWER_DIR + '/angular-touch/angular-touch.min',
-        'angular-cookie': "http://cdn.bootcss.com/angular.js/1.4.6/angular-cookies.min",
+        'angular-cookie': "//cdn.bootcss.com/angular.js/1.4.6/angular-cookies.min",
         'jquery': BOWER_DIR + '/jquery/dist/jquery.min',
         'moment': BOWER_DIR + '/moment/min/moment.min',
         //项目相关
@@ -18,17 +18,17 @@ require.config({
         'routes': './scripts/app/routes',
         'intercepter': './scripts/app/intercepter',
 
-        'site-nav': '/directives/common_pages/header',
-        'search-bar': '/directives/common_pages/search-bar',
-        'site-footer': '/directives/common_pages/site-footer',
+        'site-nav': './directives/common_pages/header',
+        'search-bar': './directives/common_pages/search-bar',
+        'site-footer': './directives/common_pages/site-footer',
         //轮播图
-        'kslider': '/directives/common_pages/slider',
-        'block-group': '/directives/pages/block_group/block_group',
-        'guess-you-like': '/directives/pages/guess_u_like/guess_u_like'
+        'kslider': './directives/common_pages/slider',
+        'block-group': './directives/pages/block_group/block_group',
+        'guess-you-like': './directives/pages/guess_u_like/guess_u_like'
     },
     map: {
         '*': {
-            'css': 'http://apps.bdimg.com/libs/require-css/0.1.8/css.min.js' // or whatever the path to require-css is
+            'css': '//apps.bdimg.com/libs/require-css/0.1.8/css.min.js' // or whatever the path to require-css is
         }
     },
     shim: {
@@ -56,7 +56,7 @@ require.config({
         },
         'app': {
             //files to be load before start
-            deps: ['angular', 'angular-ui-router', 'angular-require', 'angular-animate', 'angular-cookie', 'angular-touch', 'css!styles/reset.css', 'css!styles/base.css'],
+            deps: ['angular', 'angular-ui-router', 'angular-require', 'angular-animate', 'angular-cookie', 'angular-touch', 'css!./styles/reset.css', 'css!./styles/base.css'],
             exports: 'app'
         },
         'angular-touch': {
